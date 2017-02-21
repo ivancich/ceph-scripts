da1c5275e65c550c79eef3619a4338742c60403a
J. Eric Ivancich <ivancich@redhat.com>
2017-02-21 09:12:09 -0500

    Create tools for plotting 'combined' data -- means and stddevs.
    Signed-off-by: J. Eric Ivancich <ivancich@redhat.com>

--- combine-data.rb
+++ combine-data.rb
@@ -1,13 +1,11 @@
 #!/usr/bin/env ruby
 
-# througput 9 to 20
+$style = ARGV.shift
+$bucket_size = ARGV.shift.to_f
 
-def to_bin(v)
-  (v / 1000.0).to_i
-end
-
-def from_bin(bin)
-  bin * 1000.0
+if $style != "linear" && $style != "log" then
+  puts "Error: first arg must be 'linear' or 'log'."
+  exit 1
 end
 
 class Stats
@@ -36,7 +34,7 @@ class Stats
   end
 
   def std_dev
-    if @count > 0 then
+    if @count > 1 then
       variance = (@count * @sum_squares - (@sum * @sum)) /
         (@count * (@count - 1))
       Math.sqrt(variance)
@@ -46,11 +44,54 @@ class Stats
   end
 end
 
-ARGV.each do |a|
-  File.readlines(a).each do |l|
-    next if l =~ /^ *#/
-      a = l.split(' ')
-    
-    puts "#
+# througput 9 to 20
+
+to_bin_lin = Proc.new do |v|
+  (v / $bucket_size).to_i
+end
+
+from_bin_lin = Proc.new do |bin|
+  $bucket_size * bin + $bucket_size / 2.0
+end
+
+to_bin_log = Proc.new do |v|
+  (Math::log2(v.to_f) / $bucket_size).to_i
+end
+
+from_bin_log = Proc.new do |bin|
+  2.0 ** (bin * $bucket_size.to_f)
+end
+
+def main(argv, to_bin, from_bin)
+  latency_bins = Hash.new { |h,k| h[k] = Stats.new }
+  throughput_bins = Hash.new { |h,k| h[k] = Stats.new }
+
+  argv.each do |a|
+    File.readlines(a).each do |l|
+      next if l =~ /^ *#/
+        a = l.split(' ')
+
+      throttle_size = a[8].to_f
+      bin = to_bin.call(throttle_size)
+      throughput = a[19].to_f
+      throughput_bins[bin].add(throughput)
+      latency = a[1].to_f
+      latency_bins[bin].add(latency)
+    end
   end
+
+  puts "# combined data"
+  puts "# 1:throttle_size_bytes 2:data_point_count 3:latency_mean 4:latency_std_dev 5:throughput_mean 6:throughput_std_dev"
+  throughput_bins.keys.sort.each do |b|
+    throughput_stats = throughput_bins[b]
+    latency_stats = latency_bins[b]
+    puts "#{from_bin.call(b)} #{latency_stats.count} #{latency_stats.mean} #{latency_stats.std_dev} #{throughput_stats.mean} #{throughput_stats.std_dev}"
+  end
+end
+
+
+if $style == "linear" then
+  main(ARGV, to_bin_lin, from_bin_lin)
+else
+  main(ARGV, to_bin_log, from_bin_log)
 end
